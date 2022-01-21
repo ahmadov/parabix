1b20d66e66993be17490a9ab4d054ad29a62f75e
Elmi Ahmadov <elmi.ahmadov@tum.de>
2022-01-22 00:07:29 +0100

    Use bit matrix transpose approach to fill basis bit streams

--- tools/vgrep_block.cc
+++ tools/vgrep_block.cc
@@ -12,13 +12,23 @@
 #include "codegen/ast.h"
 #include "codegen/expression_compiler_cpp.h"
 
+#ifndef PRINT
+#define PRINT false 
+#endif
+
 void print_help(const char* name) {
   std::cerr << "usage: " << name << " [/path/to/file] [regex]" << std::endl;
 }
 
-#ifndef PRINT
-#define PRINT false 
-#endif
+void print_basis_table(std::array<uint64_t, 8>& arr, std::string_view name) {
+  for (auto& elem : arr) {
+    std::cout << std::setw(4) << std::left << name;
+    for (auto j = 0; j < 63; ++j) {
+      std::cout << ((elem >> j) & 1 ? "1" : ".");
+    }
+    std::cout << std::endl;
+  }
+}
 
 void print_table(std::vector<uint64_t>& stream, std::string_view name) {
   for (auto& elem : stream) {
@@ -27,6 +37,49 @@ void print_table(std::vector<uint64_t>& stream, std::string_view name) {
       std::cout << ((elem >> j) & 1 ? "1" : ".");
     }
     std::cout << std::endl;
+  }
+}
+
+void transpose_sse(char *inp, uint8_t *out, int n_rows) {
+  int n_cols = 8;
+#   define INP(x,y) inp[(x)*n_cols/8 + (y)/8]
+#   define OUT(x,y) out[(y)*n_rows/8 + (x)/8]
+  int rr, cc, i, h;
+  union { __m128i x; uint8_t b[16]; } tmp{};
+
+  // Do the main body in 16x8 blocks:
+  for (rr = 0; rr <= n_rows - 16; rr += 16) {
+    for (cc = 0; cc < n_cols; cc += 8) {
+      for (i = 0; i < 16; ++i) {
+        tmp.b[i] = INP(rr + i, cc);
+      }
+      for (i = 0; i < 8; ++i, tmp.x = _mm_slli_epi64(tmp.x, 1)) {
+        *reinterpret_cast<uint16_t*>(&OUT(rr,cc+i))= _mm_movemask_epi8(tmp.x);
+      }
+    }
+  }
+  if (rr == n_rows) return;
+
+  // The remainder is a block of 8x(16n+8) bits (n may be 0).
+  //  Do a PAIR of 8x8 blocks in each step:
+  for (cc = 0; cc <= n_cols - 16; cc += 16) {
+    for (i = 0; i < 8; ++i) {
+      tmp.b[i] = h = *reinterpret_cast<uint16_t const*>(&INP(rr + i, cc));
+      tmp.b[i + 8] = h >> 8;
+    }
+    for (i = 8; --i >= 0; tmp.x = _mm_slli_epi64(tmp.x, 1)) {
+      OUT(rr, cc + i) = h = _mm_movemask_epi8(tmp.x);
+      OUT(rr, cc + i + 8) = h >> 8;
+    }
+  }
+  if (cc == n_cols) return;
+
+  //  Do the remaining 8x8 block:
+  for (i = 0; i < 8; ++i) {
+    tmp.b[i] = INP(rr + i, cc);
+  } 
+  for (i = 8; --i >= 0; tmp.x = _mm_slli_epi64(tmp.x, 1)) {
+    OUT(rr, cc + i) = _mm_movemask_epi8(tmp.x);
   }
 }
 
@@ -77,18 +130,22 @@ int main(int argc, char** argv) {
 #endif
     auto to = std::min(block_size, input_size - i);
 
+    std::array<uint8_t, 64> output;
+
+    transpose_sse(input.data() + i, output.data(), 64);
+
     std::array<uint64_t, 8> basis = {0};
-    // TODO(me): use transpose matrix
-    for (auto j = 0; j < to; ++j) {
-      for (size_t k = 0; k < 8; ++k) {
-        if ((input[i + j] >> k) & 1) {
-          basis[k] |= (1ULL << j);
-        }
-      }
+    for (auto i = 7; i >= 0; --i) {
+       auto offset = i * 8;
+       for (auto j = 7; j >= 0; --j) {
+        auto current = static_cast<uint64_t>(output[offset + j]);
+        basis[7 - i] |= (current << (j * 8));
+       }
+       basis[7 - i] &= ~(1ULL << block_size);
     }
 
 #if PRINT
-    print_table(basis, "B");
+    print_basis_table(basis, "B");
 #endif
 
     std::vector<uint64_t> cc(cc_size);
