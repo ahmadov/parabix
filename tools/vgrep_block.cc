181b9de293cdb72cce838dbf6fe9a284cb0f8246
Elmi Ahmadov <elmi.ahmadov@tum.de>
2022-01-22 11:24:53 +0100

    Simplify transpose method
    The transpose method will always process 64x8 matrix, therefore some
    parts can be removed and simplified.
    
    Move arrays outside of the `for` loop to avoid construction and
    deconstruction of these arrays.
    
    Simplity `advance` operation.

--- tools/vgrep_block.cc
+++ tools/vgrep_block.cc
@@ -40,46 +40,19 @@ void print_table(std::vector<uint64_t>& stream, std::string_view name) {
   }
 }
 
-void transpose_sse(char *inp, uint8_t *out, int n_rows) {
-  int n_cols = 8;
-#   define INP(x,y) inp[(x)*n_cols/8 + (y)/8]
-#   define OUT(x,y) out[(y)*n_rows/8 + (x)/8]
-  int rr, cc, i, h;
+void transpose_sse(char *inp, uint8_t *out) {
+# define OUT(x,y) out[(y)*8 + (x)/8]
+  int rr, i, h;
   union { __m128i x; uint8_t b[16]; } tmp{};
 
   // Do the main body in 16x8 blocks:
-  for (rr = 0; rr <= n_rows - 16; rr += 16) {
-    for (cc = 0; cc < n_cols; cc += 8) {
-      for (i = 0; i < 16; ++i) {
-        tmp.b[i] = INP(rr + i, cc);
-      }
-      for (i = 0; i < 8; ++i, tmp.x = _mm_slli_epi64(tmp.x, 1)) {
-        *reinterpret_cast<uint16_t*>(&OUT(rr,cc+i))= _mm_movemask_epi8(tmp.x);
-      }
+  for (rr = 0; rr <= 48; rr += 16) {
+    for (i = 0; i < 16; ++i) {
+      tmp.b[i] = inp[rr + i];
     }
-  }
-  if (rr == n_rows) return;
-
-  // The remainder is a block of 8x(16n+8) bits (n may be 0).
-  //  Do a PAIR of 8x8 blocks in each step:
-  for (cc = 0; cc <= n_cols - 16; cc += 16) {
-    for (i = 0; i < 8; ++i) {
-      tmp.b[i] = h = *reinterpret_cast<uint16_t const*>(&INP(rr + i, cc));
-      tmp.b[i + 8] = h >> 8;
+    for (i = 0; i < 8; ++i, tmp.x = _mm_slli_epi64(tmp.x, 1)) {
+      *reinterpret_cast<uint16_t*>(&OUT(rr,i))= _mm_movemask_epi8(tmp.x);
     }
-    for (i = 8; --i >= 0; tmp.x = _mm_slli_epi64(tmp.x, 1)) {
-      OUT(rr, cc + i) = h = _mm_movemask_epi8(tmp.x);
-      OUT(rr, cc + i + 8) = h >> 8;
-    }
-  }
-  if (cc == n_cols) return;
-
-  //  Do the remaining 8x8 block:
-  for (i = 0; i < 8; ++i) {
-    tmp.b[i] = INP(rr + i, cc);
-  } 
-  for (i = 8; --i >= 0; tmp.x = _mm_slli_epi64(tmp.x, 1)) {
-    OUT(rr, cc + i) = _mm_movemask_epi8(tmp.x);
   }
 }
 
@@ -123,32 +96,34 @@ int main(int argc, char** argv) {
     expressions[i] = cc_compiler.compile(cc_list[i]);
   }
 
+  auto markers_size = cc_size + 1;
+  std::vector<uint64_t> cc(cc_size);
+  std::vector<uint64_t> marker(markers_size);
   codegen::ExpressionCompilerCpp expr_compiler_cpp;
+  std::array<uint8_t, 64> output;
+  std::array<uint64_t, 8> basis;
   for (size_t i = 0, block = 0; i < input_size; i += block_size, ++block) {
 #if PRINT
     std::cout << "processing block " << block << std::endl;
 #endif
     auto to = std::min(block_size, input_size - i);
 
-    std::array<uint8_t, 64> output;
-
-    transpose_sse(input.data() + i, output.data(), 64);
-
-    std::array<uint64_t, 8> basis = {0};
-    for (auto i = 7; i >= 0; --i) {
-       auto offset = i * 8;
-       for (auto j = 7; j >= 0; --j) {
+    transpose_sse(input.data() + i, output.data());
+    
+    for (auto i = 0; i < 8; ++i) {
+       basis[i] = 0;
+       auto offset = (7 - i) * 8;
+       for (auto j = 0; j < 8; ++j) {
         auto current = static_cast<uint64_t>(output[offset + j]);
-        basis[7 - i] |= (current << (j * 8));
+        basis[i] |= (current << (j * 8));
        }
-       basis[7 - i] &= ~(1ULL << block_size);
+       basis[i] &= ~(1ULL << block_size);
     }
 
 #if PRINT
     print_basis_table(basis, "B");
 #endif
 
-    std::vector<uint64_t> cc(cc_size);
     for (auto i = 0; i < cc_size; ++i) {
       cc[i] = expr_compiler_cpp.execute(basis, expressions[i].get());
     }
@@ -157,8 +132,6 @@ int main(int argc, char** argv) {
     print_table(cc, "CC");
 #endif
 
-    auto markers_size = cc_size + 1;
-    std::vector<uint64_t> marker(markers_size);
     marker[0] = cc[0];
     for (size_t i = 0; i < cc_size; ++i) {
       if (cc_list[i].isStar()) {
@@ -173,12 +146,11 @@ int main(int argc, char** argv) {
       } else {
         auto M = marker[i];
         M &= cc[i];
-        bool next_carry = (M >> (block_size - 1)) & 1;
         M <<= 1;
-        M &= ~(1ULL << block_size);
         M |= carry[i];
+        carry[i] = (M >> block_size) & 1;
+        M &= ~(1ULL << block_size);
         marker[i + 1] = M;
-        carry[i] = next_carry;
       }
     }
 
